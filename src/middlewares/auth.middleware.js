c878e094d2712f2d8afe917f82ca177527ccfd16
Jaffa8 <22dcs021@nith.ac.in>
2024-06-25 16:04:48 +0530

    user is getting logged out succesfully

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -8,35 +8,34 @@ import { User } from "../models/user.models.js";
 
 export const verifyJWT=asyncHandler(async(req,res,next)=>{
     try {
-        const token=req.cookies?.accessToken ||   req.header("Authorization")?.replace("Bearer ","") 
-    
+        
+        const token=req.cookies?.accessToken  ||   req.header("Authorization")?.replace("Bearer ","") 
+         console.log(token);
         if(!token){
             throw new ApiErrors(401,"Unauthorized")
         }
+        
         // verify the token
     
         const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
-    
-       const user=await User.findById(decodedToken?._id).select("-password -refreshToken")
-    
+         
+       console.log("decoded token",decodedToken);
+
+       const user=await User.findById(decodedToken?.id).select("-password -refreshToken")
+
+       console.log("user",user);
+       
        if(!user){
            throw new ApiErrors(401,"Invalid Access Token")
        }
-       req.user=user;
+      
+       req.user=user;   
+      
        next();
-    } catch (error) {
-        if (error.name === 'JsonWebTokenError') {
-            // Handle invalid token errors
-            throw new ApiErrors(401, "Invalid token");
-        } else if (error.name === 'TokenExpiredError') {
-            // Handle expired token errors
-            throw new ApiErrors(401, "Token has expired");
-        } else {
-            // For other errors, consider logging them for debugging purposes
-            console.error("Error in verifyJWT middleware:", error);
-            throw new ApiErrors(500, "An internal server error occurred");
-        }
-
+    }
+     catch (error) {
+        throw new ApiErrors(401,"Unauthorized");
+        
     
 }
 })
